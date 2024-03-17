7e13751c7dd39986192ed4b7ef97257aae4066bc
Matt <skyminds@users.noreply.github.com>
2024-03-17 10:58:10 +0100

    String consistency

--- test-connection.php
+++ test-connection.php
@@ -6,7 +6,7 @@ $db = mysqli_connect('127.0.0.1', 'user1', 'password', 'cat1.testdb1', 3306);
 print_r($db);
 
 /**
- * Testing connection with a root user
+ * Test connection with a root user
  */
 $db = mysqli_connect('127.0.0.1', 'root', 'rootpassword', null, 3306);
 print_r($db);
