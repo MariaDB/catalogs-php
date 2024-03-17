8f9dbbfc9f17fa1d1e102f5f99425c3216eb317c
Matt <skyminds@users.noreply.github.com>
2024-03-17 13:19:44 +0100

    solve phpcs errors

--- src/Catalog.php
+++ src/Catalog.php
@@ -9,6 +9,7 @@ namespace Mariadb\CatalogsPHP;
  */
 class Catalog
 {
+
     /**
      * The connection to the MariaDB server.
      *
@@ -16,7 +17,9 @@ class Catalog
      */
     private $connection;
 
-    public const MINIMAL_MARIA_VERSION = '11.0.3'; // This is too low, because this is a beta version we are developing for.
+    public const MINIMAL_MARIA_VERSION = '11.0.3';
+    // This is too low, because this is a beta version we are developing for.
+
 
     /**
      * Class constructor.
@@ -27,46 +30,47 @@ class Catalog
      * the server.
      * Default values are provided to simplify instantiation for common scenarios.
      *
-     * @param string     $db_host    The hostname or IP address of the database server. Default is 'localhost'.
-     * @param int        $db_port    The TCP/IP port of the database server. Default is 3306.
-     * @param string     $db_user    The username for the database login. Default is 'root'.
-     * @param string     $db_pass    The password for the database login. Default is an empty string.
-     * @param array|null $db_options Optional. An array of options for the server connection.
+     * @param string     $dbHost    The hostname or IP address of the database server. Default is 'localhost'.
+     * @param int        $dbPort    The TCP/IP port of the database server. Default is 3306.
+     * @param string     $dbUser    The username for the database login. Default is 'root'.
+     * @param string     $dbPass    The password for the database login. Default is an empty string.
+     * @param array|null $dbOptions Optional. An array of options for the server connection.
      *
      * @throws PDOException If a PDO error occurs during the connection attempt.
      * @throws Exception    If a general error occurs during instantiation.
      */
     public function __construct(
-        protected string $db_host = 'localhost',
-        protected int $db_port = 3306,
-        protected string $db_user = 'root',
-        protected string $db_pass = '',
-        protected ?array $db_options = null
+        protected string $dbHost='localhost',
+        protected int $dbPort=3306,
+        protected string $dbUser='root',
+        protected string $dbPass='',
+        protected ?array $dbOptions=null
     ) {
         // Connect.
         try {
             // Corrected to use the updated parameter names.
             $this->connection = new \PDO(
-                "mysql:host=$db_host;port=$db_port",
-                $db_user,
-                $db_pass,
-                $db_options
+                "mysql:host=$dbHost;port=$dbPort",
+                $dbUser,
+                $dbPass,
+                $dbOptions
             );
         } catch (\PDOException $e) {
             throw $e;
         }
 
         // Check the MariaDB version.
-        $version_query = $this->connection->query('SELECT VERSION()');
-        $version = $version_query->fetchColumn();
+        $versionQuery = $this->connection->query('SELECT VERSION()');
+        $version      = $versionQuery->fetchColumn();
 
-        if (version_compare($version, self::MINIMAL_MARIA_VERSION, '<')) {
+        if (version_compare($version, self::MINIMAL_MARIA_VERSION, '<') === true) {
             throw new Exception(
-                'The MariaDB version is too low. The minimal version is ' .
-                self::MINIMAL_MARIA_VERSION
+                'The MariaDB version is too low. The minimal version is '.self::MINIMAL_MARIA_VERSION
             );
         }
+
     }
+
 
     /**
      * Create a new catalog
@@ -80,26 +84,26 @@ class Catalog
      */
     public function create(
         string $catName,
-        string $catUser = null,
-        string $catPassword = null,
-        array $args = null
+        string $catUser=null,
+        string $catPassword=null,
+        array $args=null
     ): int {
         // Check if shell scripts are allowed to execute.
         // Might be restricted by the server.
         // Check if the Catalog name is valid.
-        if (in_array($catName, array_keys($this->show()))) {
+        if (in_array($catName, array_keys($this->show())) === true) {
             throw new Exception('Catalog name already exists.');
         }
+
         // Basically run:
         // mariadb-install-db --catalogs="list" --catalog-user=user --catalog-password[=password] --catalog-client-arg=arg
-
-        $cmd = 'mariadb-install-db --catalogs="' . escapeshellarg($catName) .
-            '" --catalog-user=' . escapeshellarg($catUser) .
-            ' --catalog-password=' . escapeshellarg($catPassword);
+        $cmd = 'mariadb-install-db --catalogs="'.escapeshellarg($catName).'" --catalog-user='.escapeshellarg($catUser).' --catalog-password='.escapeshellarg($catPassword);
         system($cmd);
 
         return $this->getPort($catName);
+
     }
+
 
     /**
      * Get the port of a catalog.
@@ -111,8 +115,10 @@ class Catalog
     public function getPort(string $catName): int
     {
         // TODO: wait for the functionality to be implemented in the server.
-        return $port ?? 0;
+        return ($port ?? 0);
+
     }
+
 
     /**
      * Get all catalogs.
@@ -122,13 +128,16 @@ class Catalog
     public function show(): array
     {
         $catalogs = [];
-        $results = $this->connection->query('SHOW CATALOGS');
+        $results  = $this->connection->query('SHOW CATALOGS');
         foreach ($results as $row) {
             // For now, we just return the default port for all catalogs.
-            $catalogs[$row['name']] = $this->db_port;
+            $catalogs[$row['name']] = $this->dbPort;
         }
+
         return $catalogs;
+
     }
+
 
     /**
      * Drop a catalog.
@@ -143,17 +152,19 @@ class Catalog
     public function drop(string $catName): bool
     {
         $this->connection->query(
-            'DROP CATALOG ' . $this->connection->quote($catName)
+            'DROP CATALOG '.$this->connection->quote($catName)
         );
 
-        if ($this->connection->errorCode()) {
+        if ($this->connection->errorCode() === true) {
             throw new Exception(
-                'Error dropping catalog: ' .
-                $this->connection->errorInfo()[2]
+                'Error dropping catalog: '.$this->connection->errorInfo()[2]
             );
         }
+
         return true;
+
     }
+
 
     /**
      * This is out of scope, that's why it's private.
@@ -163,6 +174,10 @@ class Catalog
      */
     private function alter()
     {
+        // PHPCS:ignore
         // TODO implement the ALTER CATALOG command.
+
     }
+
+
 }
