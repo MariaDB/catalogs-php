c160065b6efef42d0307102a67c65f5e687dbe32
Matt <skyminds@users.noreply.github.com>
2024-03-17 10:57:07 +0100

    formatting

--- implementations-dummy-code.php
+++ implementations-dummy-code.php
@@ -1,14 +1,11 @@
 <?php
 
-/****
- * Use case 1: Create a new catalog and connect it to a wp install.
- */
-
 use Mariadb\CatalogsPHP\Catalog;
 
-// Creating a new catalog
-$cat =  new Catalog();
-$catPort = $cat->create( 'wp_1' );
+/**
+ * Use case 1: Create a new catalog and connect it to a wp install.
+ */
+$cat = new Catalog();
+$catPort = $cat->create('wp_1');
 // Using PDO, Create a DB and user in the collection using the $catPort.
 // Set DB_NAME with the "name:$catPort"
-
